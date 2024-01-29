8032c71b7b1c427328d17a9e62d65ae2faa9a514
Edson Almeida <edson.almeidajl@hotmail.com>
2024-01-29 16:48:58 -0300

    Projeto finalizado.

--- script.js
+++ script.js
@@ -10,3 +10,69 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+function renderElements(arrObject) {
+  const ul = document.querySelector("ul");
+  ul.innerHTML = "";
+  for(let i = 0; i < arrObject.length; i++) {
+    const follow = createTaskItem(arrObject[i]);
+    ul.append(follow);
+  }
+   
+  return ul;
+}
+renderElements(tasks);
+
+function createTaskItem (object) {
+  const liItem = document.createElement("li");
+  const divItem = document.createElement("div");
+  const spanItem = document.createElement("span");
+  const pItem = document.createElement("p");
+  const buttonItem = document.createElement("button");
+
+  liItem.classList.add("task__item");
+  divItem.classList.add("task-info__container");
+  pItem.innerText = object.title;
+  buttonItem.classList.add("task__button--remove-task");
+  spanItem.classList.add("task-type");
+
+  if (object.type.toLowerCase() == "urgente") {
+    spanItem.classList.add("span-urgent");
+  }
+  else if (object.type.toLowerCase() == "importante") {
+    spanItem.classList.add("span-important");
+  }
+  else {
+    spanItem.classList.add("span-normal");
+  }
+
+  divItem.append(spanItem, pItem);
+  liItem.append( divItem, buttonItem);
+
+  buttonItem.addEventListener("click", function() {
+    const index = tasks.indexOf(object);
+    tasks.splice(index, 1);
+    renderElements(tasks);
+  })
+  return liItem;
+}
+
+function currentTask () {
+  const btnAddTask = document.querySelector(".form__button--add-task");
+
+    btnAddTask.addEventListener("click", function(event) {
+    event.preventDefault();
+    
+    const includeTask = document.querySelector(".form__input--text");
+    const choiceTask = document.querySelector(".form__input--priority");
+
+    let listTask = {
+      title: includeTask.value,
+      type: choiceTask.value
+    };
+
+    tasks.push(listTask);
+
+    renderElements(tasks);
+  })
+}
+currentTask();
